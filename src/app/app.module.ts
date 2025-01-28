64e70dacd7dc00358ad79a746151e952db63f1c6
GRANATIN, Angelo <angeloidgranatin@gmail.com>
2025-01-28 18:03:08 +0800

    added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { PostCreateComponent } from './post/post-create/post.create.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PostCreateComponent } from "./post/post-create/post.create.component";
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PostCreateComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-  ],
+    BrowserAnimationsModule,
+    PostCreateComponent
+],
   providers: [],
   bootstrap: [AppComponent]
 })
