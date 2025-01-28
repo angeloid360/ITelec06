7039c3a4c49c9846d847d116edd763227cd6a17c
GRANATIN, Angelo <112195813+angeloid360@users.noreply.github.com>
2025-01-28 18:03:55 +0800

    added (#1)

--- src/app/post/post-create/post.create.component.ts
+++ src/app/post/post-create/post.create.component.ts
@@ -1,9 +1,18 @@
 import { Component } from "@angular/core";
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
     selector: 'post-create',
     templateUrl: './post.create.component.html',
-    styleUrls: ['./post.create.component.css']
+    styleUrls: ['./post.create.component.css'],
+    standalone:true,
+    imports: [
+        MatButtonModule,
+        MatIconModule,
+        MatDividerModule,
+      ],
 })
 
 export class PostCreateComponent{
